a71a4c2447798ec163e6ddefd26756b99e5868ec
James <dickfala@gmail.com>
2017-11-22 21:45:11 +0800

    Initial Commit

---
+++ NibDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NibDemo
+//
+//  Created by cyy on 2017/11/22.
+//  Copyright © 2017年 james chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
